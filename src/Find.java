6185c90a8f19a103a73c26e7717fe64212b88da8
LluisMutA <lluis.mut@gmail.com>
2024-03-21 12:59:58 +0100

    Captures1 5 passats

--- src/Find.java
+++ src/Find.java
@@ -22,11 +22,18 @@ public class Find {
         return false;
     }
 
-    private boolean tryMatch(Pattern pat, int pos){
+        private boolean tryMatch(Pattern pat, int pos){
         for (int j = 0; j < pat.components.size(); j++) {
             if (j + pos >= this.text.length()) {
                 if (pat.components.get(j).tipo == Component.tipoComponent.EOL){ // Comprovam que sigui un EOL ---> j perque no surti del limit
-                    return true;
+                    if(j == pat.components.size() -1) { // mira si es el darrer
+                        //  System.out.println("final");
+                        return true;
+                    }else{
+                        // System.out.println("eol pero no final");
+
+                        return false;}
+
                 }
                 return false;
             }
@@ -70,17 +77,77 @@ public class Find {
         return true;
     }
 
+
     public String capture(String pat) {
         Pattern p = new Pattern(pat);
         for (int i = 0; i < this.text.length(); i++) {
             if (tryMatch(p, i)) {
-                int end = i + pat.length() -1; // -1?
-                return this.text.substring(i, end);
+                int end = i + pat.length();
+                if (p.components.get(p.components.size()-1).tipo == Component.tipoComponent.EOL) {
+                    end = this.text.length(); // Si es eol
+                }else {
+                    end = Math.min(end, this.text.length()-1); //
+//                int end = i + pat.length(); // -1 si el metode no es eol pero no el darrer
+//                return this.text.substring(i, end);
+                }return this.text.substring(i, end);
             }
         }
         return null;
     }
 }
 
-
+//    private boolean tryMatch(Pattern pat, int pos){
+//        for (int j = 0; j < pat.components.size(); j++) {
+//            if (j + pos >= this.text.length()) {
+//                if (pat.components.get(j).tipo == Component.tipoComponent.EOL){ // Comprovam que sigui un EOL ---> j perque no surti del limit
+//                    if(j == pat.components.size() -1) { // mira si es el darrer
+//                        //  System.out.println("final");
+//                        return true;
+//                    }else{
+//                        // System.out.println("eol pero no final");
+//
+//                        return false;}
+//
+//                }
+//                return false;
+//            }
+//
+//            char c = this.text.charAt(j + pos);
+//            Component component = pat.components.get(j);
+//
+//            if (component.tipo == Component.tipoComponent.BOL){
+//                if (pos != 0){ // Canviam el tipos si no es BOL a Char, corregir pattern
+//                    component.tipo = Component.tipoComponent.CHAR;
+//                }
+//                pos--; // j- pos?
+//            }
+//            if (component.tipo == Component.tipoComponent.EOL){
+//                if (j != this.text.length()-1){  // Si no esta al final tornam fals directe
+//                    return false;
+//                }
+//            }
+//            if (component.tipo == Component.tipoComponent.CHAR){
+//                if (c != component.ch) return false;
+//            }
+//            if (component.tipo == Component.tipoComponent.SET){
+//                boolean match = false; // Comprovam si coincideix per donar true i acabar el bucle
+//                for (int k = 0; k < component.set.length(); k++) {
+//                    char comprovaChar = component.set.charAt(k);
+//                    if (comprovaChar == '-' && k != 0 && k != component.set.length() - 1) {
+//                        char esInici = component.set.charAt(k - 1);
+//                        char esFinal = component.set.charAt(k + 1);
+//                        if (c >= esInici && c <= esFinal) {
+//                            match = true;
+//                            break;
+//                        }
+//                    } else if (c == comprovaChar) {
+//                        match = true;
+//                        break;
+//                    }
+//                }
+//                if (!match) return false;
+//            }
+//        }
+//        return true;
+//    }
 
